3e73558e379b1ac89b7d6dba7531c678879f24ae
wendypam <wendypamelapp@gmail.com>
2024-12-06 13:30:21 -0500

    Actualización de views 3

--- views/roles.php
+++ views/roles.php
@@ -921,7 +921,7 @@
                 },
                 dataType: "json",
                 success: function(r) {
-                    let name = [1, 3, 4, 5, 6, 7, 9, 10,11,13, 14, 16, 17, 18, 19];
+                    let name = [1, 3, 4, 5, 6, 7, 9, 10,11,13, 14, 16, 17, 18, 19, 20, 21];
                     let data = [];
                     let isBodega = true;
                     let isCompras = true;
